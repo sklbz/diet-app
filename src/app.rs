3b1d0efb0b7cf45a0bd66b77fb56ad790c48948e
sklbz <sokol.bozanic@tumo.world>
2025-08-16 10:05:59 +0200

    adding frontend

---
+++ src/app.rs
@@ -0,0 +1,55 @@
+use leptos::task::spawn_local;
+use leptos::{ev::SubmitEvent, prelude::*};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize, Deserialize)]
+struct GreetArgs<'a> {
+    name: &'a str,
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    let (name, set_name) = signal(String::new());
+    let (greet_msg, set_greet_msg) = signal(String::new());
+
+    let update_name = move |event| {
+        let value = event_target_value(&event);
+        set_name.set(value);
+    };
+
+    let greet = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        spawn_local(async move {
+            let name = name.get_untracked();
+            if name.is_empty() {
+                return;
+            }
+
+            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name }).unwrap();
+            let new_msg = invoke("greet", args).await.as_string().unwrap();
+            set_greet_msg.set(new_msg);
+        });
+    };
+
+    view! {
+        <main class="container">
+            <form class="row" on:submit=greet>
+                <input
+                    type="text"
+                    id="greet-input"
+                    placeholder="Enter a food..."
+                    on:input=update_name
+                />
+                <button type="submit">"Add food"</button>
+            </form>
+            <p>{ move || greet_msg.get() }</p>
+        </main>
+    }
+}
